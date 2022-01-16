ee856e36a1c0183188b2eac62dc6cf53bb51b72a
DanFeLoGar <danypolo18@hotmail.com>
2022-01-16 17:52:25 -0500

    initial commit

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { SafeAreaView, StatusBar } from 'react-native';
+
+import { styles } from './src/theme/appTheme';
+import { CalculadoraScreen } from './src/screens/CalculadoraScreen';
+
+const App = () => {
+  return (
+    <SafeAreaView style={ styles.fondo }>
+      <StatusBar
+        backgroundColor='black'
+        barStyle='light-content'
+      />
+      <CalculadoraScreen />
+    </SafeAreaView>
+  )
+}
+
+export default App;
